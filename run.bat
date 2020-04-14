4fea7e9e7a50427a0fad98215d5d040a4a3279bd
Ngo Chanh Anh Minh <ncaminh.study@gmail.com>
2020-04-14 12:37:04 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 @echo off
+python log_in.py
+cls
 python manage.py migrate
 python manage.py makemigrations
-cls
-python log_in.py
 cls
 explorer "http://127.0.0.1:8000/"
 python manage.py runserver
